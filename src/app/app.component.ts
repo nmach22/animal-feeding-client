77c7b83397a3c95ace3b9b42a01ff645e12be0a8
nikamatcharadze <nmach22@freeuni.edu.ge>
2025-02-06 18:38:15 +0400

    feeding, points are updating, pig's status is changing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {AnimalService} from './services/animal.service';
+import {NgForOf, NgIf} from '@angular/common';
+
+
+interface Animal {
+  id: number;
+  name: string;
+  type: string;
+  gratitude_count: number;
+  image_url: string;
+  update_date: string;
+}
+
+interface MasterStatus {
+  status: string;
+  points: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'animal-feeding-client';
+  animals: Animal[] = [];
+  masterAnimal: Animal | null = null;
+  otherAnimals: Animal[] = [];
+  masterStatus: MasterStatus | null = null;
+
+  constructor(private animalService: AnimalService) {
+  }
+
+  ngOnInit() {
+    this.fetchAnimals();
+    this.fetchMasterStatus();
+  }
+
+  fetchAnimals() {
+    this.animalService.getAnimals().subscribe({
+      next: (response) => {
+        this.animals = response.animals;
+
+        // Separate MASTER animal and other animals
+        this.masterAnimal = this.animals.find(animal => animal.type === 'MASTER') || null;
+        this.otherAnimals = this.animals.filter(animal => animal.type !== 'MASTER');
+      },
+      error: (error) => console.error('Error fetching animals:', error),
+    });
+  }
+
+  fetchMasterStatus() {
+    // Fetch status from the given URL for the MASTER animal
+    this.animalService.getMasterStatus().subscribe({
+      next: (response: MasterStatus) => {
+        // Now `response` is typed as MasterStatus, and we can safely access `status`
+        this.masterStatus = response; // Store the full response
+      },
+      error: (error) => console.error('Error fetching MASTER status:', error),
+    });
+  }
+
+
+  feedAnimal(animalId: number) {
+    this.animalService.feedAnimal(animalId).subscribe({
+      next: (response: MasterStatus) => {
+        // alert(`Animal ${animalId} has been fed! 🥕`);
+        this.masterStatus = response;
+        this.fetchAnimals();
+      },
+      error: (error) => alert(`Error feeding animal: ${error.message}`),
+    });
+  }
 }
